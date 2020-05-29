a531644e51a9514a0d1d9d12c565abf62403778a
SarahScottA7A <66109255+SarahScottA7A@users.noreply.github.com>
2020-05-29 12:26:14 +0530

    init

---
+++ precache-manifest.e231f26a52a719c20997dd8040c6a0c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce9342eccd43fad5da80e9cbec504ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da7a5b899c5930d30ce5",
+    "url": "/static/css/main.751e7336.chunk.css"
+  },
+  {
+    "revision": "31adef8bdbb162acf61b",
+    "url": "/static/js/2.64148044.chunk.js"
+  },
+  {
+    "revision": "da7a5b899c5930d30ce5",
+    "url": "/static/js/main.d20fe8d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
